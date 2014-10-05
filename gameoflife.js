9080ce86eaed956c66681e6097abb404d73129bf
Gene Connolly <connolly.gene@gmail.com>
2014-10-05 16:29:14 -0400

    JsHint, create IIFE module

--- gameoflife.js
+++ gameoflife.js
@@ -1,127 +1,139 @@
-function evolve(livingCells) {
-    var cellsToInspect = {},    //Set
-        result = {},            //Set
-        cell,
-        cellName;
-    
-    for(cellName in livingCells) {
-        cell = livingCells[cellName];
-        
-        [
-            [ cell[0],      cell[1]     ],
-            [ cell[0],      cell[1] - 1 ],
-            [ cell[0],      cell[1] + 1 ],
-            [ cell[0] + 1,  cell[1]     ],
-            [ cell[0] + 1,  cell[1] - 1 ],
-            [ cell[0] + 1,  cell[1] + 1 ],
-            [ cell[0] - 1,  cell[1]     ],
-            [ cell[0] - 1,  cell[1] - 1 ],
-            [ cell[0] - 1,  cell[1] + 1 ]
-        ].forEach(function (newCell) {
-            cellsToInspect[ newCell.join() ] = newCell;
-        })
-    }
-    
-    for(cellName in cellsToInspect) {
-        cell = cellsToInspect[cellName];
-        
-        if(determineFateOfCell(livingCells, cell)) {
-            result[ cell.join() ] = cell;
-        }
-    }    
+(function () {
+	function evolve(livingCells) {
+	    var cellsToInspect = {},    //Set
+	        result = {},            //Set
+	        cell,
+	        cellName;
+	    
+	    for(cellName in livingCells) {
+	    	if (livingCells.hasOwnProperty(cellName)) {
+		        cell = livingCells[cellName];
+		        
+		        [
+		            [ cell[0],      cell[1]     ],
+		            [ cell[0],      cell[1] - 1 ],
+		            [ cell[0],      cell[1] + 1 ],
+		            [ cell[0] + 1,  cell[1]     ],
+		            [ cell[0] + 1,  cell[1] - 1 ],
+		            [ cell[0] + 1,  cell[1] + 1 ],
+		            [ cell[0] - 1,  cell[1]     ],
+		            [ cell[0] - 1,  cell[1] - 1 ],
+		            [ cell[0] - 1,  cell[1] + 1 ]
+		        ].forEach(function (newCell) {
+		            cellsToInspect[ newCell.join() ] = newCell;
+		        });
+	    	}
+	    }
+	    
+	    for(cellName in cellsToInspect) {
+	    	if (cellsToInspect.hasOwnProperty(cellName)) {
+		        cell = cellsToInspect[cellName];
+		        
+		        if(determineFateOfCell(livingCells, cell)) {
+		            result[ cell.join() ] = cell;
+		        }
+	    	}
+	    }    
 
-    return result;
-}
+	    return result;
+	}
 
-function determineFateOfCell(livingCells, cell) {
-    var livingNeighbors = getNumberOfNeighbors(livingCells, cell);
+	function determineFateOfCell(livingCells, cell) {
+	    var livingNeighbors = getNumberOfNeighbors(livingCells, cell);
 
-    return livingCells[ cell.join() ]
-        ? (livingNeighbors == 2 || livingNeighbors == 3)
-        : livingNeighbors == 3;
-}
+	    return livingCells[ cell.join() ] ? 
+	      	(livingNeighbors == 2 || livingNeighbors == 3)
+	        : livingNeighbors == 3;
+	}
 
-// return the number of living cells adjacent to the provided cell
-function getNumberOfNeighbors(livingCells, cell) {
-    return [
-        [ cell[0],      cell[1] - 1 ],
-        [ cell[0],      cell[1] + 1 ],
-        [ cell[0] + 1,  cell[1]     ],
-        [ cell[0] + 1,  cell[1] - 1 ],
-        [ cell[0] + 1,  cell[1] + 1 ],
-        [ cell[0] - 1,  cell[1]     ],
-        [ cell[0] - 1,  cell[1] - 1 ],
-        [ cell[0] - 1,  cell[1] + 1 ]
-    ].reduce(function (sum, current) {
-        return livingCells[ current.join() ]
-            ? sum + 1
-            : sum;
-    }, 0);
-}
+	// return the number of living cells adjacent to the provided cell
+	function getNumberOfNeighbors(livingCells, cell) {
+	    return [
+	        [ cell[0],      cell[1] - 1 ],
+	        [ cell[0],      cell[1] + 1 ],
+	        [ cell[0] + 1,  cell[1]     ],
+	        [ cell[0] + 1,  cell[1] - 1 ],
+	        [ cell[0] + 1,  cell[1] + 1 ],
+	        [ cell[0] - 1,  cell[1]     ],
+	        [ cell[0] - 1,  cell[1] - 1 ],
+	        [ cell[0] - 1,  cell[1] + 1 ]
+	    ].reduce(function (sum, current) {
+	        return livingCells[ current.join() ] ?
+	          	sum + 1
+	            : sum;
+	    }, 0);
+	}
 
-// render living cells in a bounding rectangle
-function draw(livingCells) {
-    var x, y,
-        minX = getMinTuple(livingCells, 0),
-        minY = getMinTuple(livingCells, 1),
-        maxX = getMaxTuple(livingCells, 0),
-        maxY = getMaxTuple(livingCells, 1),
-        grid = '\n';
+	// render living cells in a bounding rectangle
+	function draw(livingCells) {
+	    var x, y,
+	        minX = getMinTuple(livingCells, 0),
+	        minY = getMinTuple(livingCells, 1),
+	        maxX = getMaxTuple(livingCells, 0),
+	        maxY = getMaxTuple(livingCells, 1),
+	        grid = '\n';
 
-    for (y = minY; y <= maxY; y++) {
-        for (x = minX; x <= maxX; x++) {
-            grid += livingCells[ [x, y].join() ]
-                ? '*'
-                : '-';
-        }
+	    for (y = minY; y <= maxY; y++) {
+	        for (x = minX; x <= maxX; x++) {
+	            grid += livingCells[ [x, y].join() ] ? 
+	              	'*'
+	                : '-';
+	        }
 
-        grid += '\n';
-    };
+	        grid += '\n';
+	    }
 
-    return grid;
-}
+	    return grid;
+	}
 
-function reduceTupleSet(tupleSet, reduce, initial) {
-    var result = initial,
-        tupleString;
+	function reduceTupleSet(tupleSet, reduce, initial) {
+	    var result = initial,
+	        tupleString;
 
-    for (tupleString in tupleSet) {
-        result = reduce(result, tupleSet[tupleString]);
-    };
+	    for (tupleString in tupleSet) {
+			if (tupleSet.hasOwnProperty(tupleString)) {
+	        	result = reduce(result, tupleSet[tupleString]);
+	    	}
+	    }
 
-    return result;
-}
+	    return result;
+	}
 
-function getMaxTuple(tupleSet, index) {
-    return reduceTupleSet(
-        tupleSet,
-        function (previous, current) {
-            return current[index] > previous
-                ? current[index]
-                : previous;
-        },
-        Number.MIN_SAFE_INTEGER
-    );
-}
+	function getMaxTuple(tupleSet, index) {
+	    return reduceTupleSet(
+	        tupleSet,
+	        function (previous, current) {
+	            return current[index] > previous ? 
+	              	current[index]
+	                : previous;
+	        },
+	        Number.MIN_SAFE_INTEGER
+	    );
+	}
 
-function getMinTuple(tupleSet, index) {
-    return reduceTupleSet(
-        tupleSet,
-        function (previous, current) {
-            return current[index] < previous
-                ? current[index]
-                : previous;
-        },
-        Number.MAX_SAFE_INTEGER
-    );
-}
+	function getMinTuple(tupleSet, index) {
+	    return reduceTupleSet(
+	        tupleSet,
+	        function (previous, current) {
+	            return current[index] < previous ? 
+	              	current[index]
+	                : previous;
+	        },
+	        Number.MAX_SAFE_INTEGER
+	    );
+	}
 
-function createTupleSetFromArray(arr) {
-    return arr.reduce(function (tupleSet, tuple) {
-        tupleSet[tuple.join()] = tuple;
-        return tupleSet;
-    }, {});
-}
+	function createTupleSetFromArray(arr) {
+	    return arr.reduce(function (tupleSet, tuple) {
+	        tupleSet[tuple.join()] = tuple;
+	        return tupleSet;
+	    }, {});
+	}
+
+	window.evolve = evolve;
+	window.draw = draw;
+	window.createTupleSetFromArray = createTupleSetFromArray;
+}())
 
 var slider = createTupleSetFromArray([
         [1, 2],
